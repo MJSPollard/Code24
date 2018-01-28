ffc3bd7c9e593a3a0d3f908e7e5a73a489e40580
Matt Sagen <matthewpsagen@gmail.com>
2018-01-28 00:47:43 -0700

    added popup_menu

--- app/src/main/java/com/example/michael/doyouknowdeway/MainActivity.java
+++ app/src/main/java/com/example/michael/doyouknowdeway/MainActivity.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton startButton;
+    private ImageButton startButton, settingsButton;
     private MediaPlayer deway;
 
     @Override
@@ -28,7 +31,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //initialize buttons
         startButton = findViewById(R.id.imageButton2);
+        settingsButton = findViewById(R.id.imageButton4);
         startButton.setOnClickListener(this);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, settingsButton);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, "" + menuItem.getTitle(),Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
 
         deway = MediaPlayer.create(this, R.raw.do_u_kno_de_way);
     }
